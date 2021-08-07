7bd1034c58b386178c774bbbb4278ddcc1a05aea
Brenno Rodrigues de Assis <brennorassis@gmail.com>
2021-08-07 14:49:56 -0300

    Refactoring de class para function

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,28 +1,14 @@
-import { Component } from 'react';
+import { useState,useEffect } from 'react';
 import ReactModal from 'react-modal';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+const Modal = ({isOpen,children,setIsOpen}) =>{
+  const [modalStatus,setModalStatus] = useState(isOpen)
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
+  useEffect(() => {
+    if (modalStatus !== isOpen) {
+      setModalStatus(isOpen)
     }
-  }
-
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
+  },[isOpen])
 
     return (
       <ReactModal
@@ -52,7 +38,6 @@ class Modal extends Component {
         {children}
       </ReactModal>
     );
-  }
 };
 
 export default Modal;
